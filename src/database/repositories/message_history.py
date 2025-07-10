4bb9b195e9fe50d8e4800f0d11a167fdcf0c3cc7
GadCoder <german.ampuero@unmsm.edu.pe>
2025-07-10 17:16:40 -0500

    fix: change from sc to desc order in message history, and revert the change made in expenses by accident, bigger ups

--- src/database/repositories/message_history.py
+++ src/database/repositories/message_history.py
@@ -1,5 +1,6 @@
 import logging
 
+from sqlalchemy import desc
 from sqlalchemy.orm import Session
 
 from database.models.message_history import MessageHistory
@@ -8,7 +9,9 @@ from database.schemas.message_history import MessageHistoryCreate
 logger = logging.getLogger(__name__)
 
 
-def create_message_history(db: Session, message_history: MessageHistoryCreate) -> MessageHistory:
+def create_message_history(
+    db: Session, message_history: MessageHistoryCreate
+) -> MessageHistory:
     logger.debug(f"Creating message history for user_id: {message_history.user_id}")
     db_message_history = MessageHistory(**message_history.model_dump())
     db.add(db_message_history)
@@ -19,4 +22,11 @@ def create_message_history(db: Session, message_history: MessageHistoryCreate) -
 
 def get_message_history_by_user_id(db: Session, user_id: int) -> list[MessageHistory]:
     logger.debug(f"Getting message history for user_id: {user_id}")
-    return db.query(MessageHistory).filter(MessageHistory.user_id == user_id, MessageHistory.sender_type == "USER").order_by(MessageHistory.created_at).limit(10).all()
+    messages = (
+        db.query(MessageHistory)
+        .filter(MessageHistory.user_id == user_id, MessageHistory.sender_type == "USER")
+        .order_by(desc(MessageHistory.created_at))
+        .limit(10)
+        .all()
+    )
+    return list(reversed(messages))

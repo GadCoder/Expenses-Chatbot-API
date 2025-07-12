bed6c0fd077b14d33d0a444a2f9767562f2cfeed
GadCoder <german.ampuero@unmsm.edu.pe>
2025-07-12 16:58:21 -0500

    feat: improve general prompt

--- src/services/messages/enrich_prompt.py
+++ src/services/messages/enrich_prompt.py
@@ -10,44 +10,37 @@ from database.repositories.expense_category import get_user_expense_categories
 def enrich_prompt(
     db: Session, message: str, user_id: int, message_history: list[MessageHistory]
 ) -> str:
-    history_str = ""
-    if message_history:
-        history_str = (
-            "MESSAGE HISTORY: This are the last 10 messages from oldest no newer\n"
-        )
-        for msg in message_history:
-            history_str += f"- {msg.sender_type}: {msg.message}\n"
-        history_str = clean_history_text(history_text=history_str)
+    history_str = create_message_history_str(message_history=message_history)
     user_categories = get_user_expense_categories(db=db, user_id=user_id)
-    if not user_categories:
-        content = """Currently, there's no registered expenses categories. 
-        You'll have to define a new one based on the information from the request.
-        Some examples are:
-            - 'comida': anything food or drinks related
-            - 'transporte': like bus ticket pricing or taxis
-            - 'entretenmiento': anything related to entertaiment
-        - The category should be in lowercase.
-        - It should be an atomic word or short phrase in Spanish, such as: "comida", "entretenimiento", "transporte", "gastos fijos".
-        Careful when defining a a new expense category. Take into account the full sentence for it.
-        For example, 'Comida para mi mascota' at first could seems like 'food', but is actually 'pet'
-        """
-    else:
-        categories_str = ", ".join([str(c.name) for c in user_categories])
-        content = f"""This are the existing expense categories registered for this user:
-                    [{categories_str}]"""
+    user_categories_text = create_user_categories_text(user_categories=user_categories)
 
-    enriched_prompt = f"""This is the main content of this request: \n
+    enriched_prompt = f"""
+        This is the main content of this request: \n
             - {message}\n
-        Use the main content of the current request to decide which function to choose.
-        If the main request is ambiguous or lacks enough detail, refer to the message history provided below to add context.
-
-        For example, if the current request is “Ahora de ropa” and the previous message was “Dame el listado de gastos de comida”, it’s likely the user now wants the list of expenses for clothes.
-        This is just one example of how to use message history to improve understanding.
-        
-        Important: Always prioritize the main content of the current request. Only use the message history to support or clarify the main request when necessary
-        \n{history_str}\n{content}"""
+        Use the main content of the current request to determine which function to call.
+        If the request is ambiguous or lacks sufficient detail, refer to the message history provided below to add context.
+        For example, if the current request is “Ahora de ropa” and the previous message was “Dame el listado de gastos de comida”, it's likely the user now wants the list of expenses for clothes.
+        You can also extract details like time period from the previous messages. For instance, if the earlier request specified a time frame (e.g., “de los últimos 7 días”), and the new request omits it, you may use the prior time frame to fill in the gap.
+        This is just one example of how message history can help improve understanding.
+        Important: Always prioritize the main content of the current request. Use the message history only when necessary to clarify or supplement the intent.
+        \n{history_str}
+        \n{user_categories_text}
+        """
 
     return textwrap.dedent(text=enriched_prompt)
+
+
+def create_message_history_str(message_history: list[MessageHistory]) -> str:
+    if not message_history:
+        return ""
+    history_str = ""
+    history_str = (
+        "MESSAGE HISTORY: This are the last 10 messages from oldest no newer\n"
+    )
+    for index, msg in enumerate(message_history):
+        history_str += f"{index + 1}. {msg.sender_type}: {msg.message}\n"
+    history_str = clean_history_text(history_text=history_str)
+    return history_str
 
 
 def clean_history_text(history_text: str) -> str:
@@ -81,3 +74,23 @@ def clean_history_text(history_text: str) -> str:
     text = re.sub(r"[^\w\s]", "", text)
 
     return text
+
+
+def create_user_categories_text(user_categories: list) -> str:
+    if not user_categories:
+        content = """Currently, there's no registered expenses categories. 
+        You'll have to define a new one based on the information from the request.
+        Some examples are:
+            - 'comida': anything food or drinks related
+            - 'transporte': like bus ticket pricing or taxis
+            - 'entretenmiento': anything related to entertaiment
+        - The category should be in lowercase.
+        - It should be an atomic word or short phrase in Spanish, such as: "comida", "entretenimiento", "transporte", "gastos fijos".
+        Careful when defining a a new expense category. Take into account the full sentence for it.
+        For example, 'Comida para mi mascota' at first could seems like 'food', but is actually 'pet'
+        """
+    else:
+        categories_str = ", ".join([str(c.name) for c in user_categories])
+        content = f"""This are the existing expense categories registered for this user:
+                    [{categories_str}]"""
+    return content
